be7bb91ca88279a8a99da2b0d663ba1c44b88a01
irsandahpr <irsandahpr@gmail.com>
2022-09-27 13:58:16 +0700

    tugas pertemuan 3

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo"Nama saya Irsanda Hatta
+			Saya tinggal di Pamulang
+			olahraga yang saya sukai adalah
+			Bulutangkis";
+			
+	}
+}
